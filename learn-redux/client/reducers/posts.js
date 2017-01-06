4d80428952fd9953bf97d821a83c3f08a5a19ec7
Sprazzeus <sprazzeus@gmail.com>
2017-01-06 18:43:39 +0800

    updated Photo component with increment function and post reducer

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -6,9 +6,19 @@
 // every reducer will run every time there is an action dispatched. whether you update the state after an action is up to you.
 
 function posts(state = [], action) {
-  console.log('The post will change')
-  console.log(state, action)
-  return state
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('Incrementing likes!')
+      const i = action.index
+      return [
+        ...state.slice(0, i), // before one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1) // after the one we are updating
+      ]
+
+    default:
+      return state
+  }
 }
 
 export default posts
